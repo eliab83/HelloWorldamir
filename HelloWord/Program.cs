59f78aaff848dd90d91bed04f611125927f8dffc
elia <eliababaali@gmail.com>
2026-07-30 15:37:29 +0330

    add full name page

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -4,9 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("My first Git project");
-            System.Console.WriteLine("first cross repository test");
+            Console.WriteLine("Hi! pleas enter your first name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("now enter your last name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("how old are you??");
+            string age = Console.ReadLine();
+            Console.WriteLine("you entered: " + firstName + lastName);
+            Console.WriteLine("and you are " + age + " years old");
+            Console.ReadKey();
+
 
         }
     }
